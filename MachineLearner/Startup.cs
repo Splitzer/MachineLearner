978ded1d597d152e1fd05dc24d4a682aa638cc8a
Splitzer <n.gianno.spl@gmail.com>
2016-10-16 10:31:24 +0100

    first commit

---
+++ MachineLearner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MachineLearner.Startup))]
+namespace MachineLearner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
