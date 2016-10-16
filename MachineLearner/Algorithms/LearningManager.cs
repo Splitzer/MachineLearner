73d203b665eaac50ee8b55e0b6b7c7f2051845cd
Splitzer <n.gianno.spl@gmail.com>
2016-10-16 10:51:14 +0100

    Starting the structuring

---
+++ MachineLearner/Algorithms/LearningManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MachineLearner.Algorithms
+{
+    public class LearningManager
+    {
+        public LearningManager()
+        {
+
+        }
+
+        public float[] LinearRegression(float[][] X, float[] Y)
+        {
+            float[] Ypred;
+
+            Ypred = Y;
+
+            return Ypred;
+        }
+    }
+}
