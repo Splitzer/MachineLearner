38da3e405c1088987c99c0d3d52e0a812a8caaf8
Nick Gianno <n.gianno.spl@gmail.com>
2017-02-27 07:59:06 +0000

    Created the datasetService and polished the front end further.

---
+++ MachineLearner/Scripts/Angular Controllers/datasetService.js
@@ -0,0 +1,63 @@
+﻿// machineRepository.js
+(function () {
+    'use strict';
+    "ngInject";
+    var machineRepository = function ($http, $q, baseUrl) {
+        var machineList = [];
+        var getList = function () {
+            var deferred = $q.defer();
+            $http.post(baseUrl + 'Dataset/GetDataset')
+                .then(function (result) {
+                    // Successful
+                    angular.copy(result.data, machineList);
+                    deferred.resolve();
+                },
+                    function (result) {
+                        // Error
+                        deferred.reject(result);
+                    });
+            return deferred.promise;
+        };
+        var getMachine = function (id) {
+            var deferred = $q.defer();
+            $http.post(baseUrl + 'Dataset/GetTuple' + id.trim())
+                .success(deferred.resolve)
+                .error(deferred.reject);
+            return deferred.promise;
+        }
+        var filterMachines = function (filterCriteria) {
+            if (filterCriteria != null) {
+                filterCriteria.filters = [];
+                for (var key in filterCriteria.fieldFilters) {
+                    var obj = filterCriteria.fieldFilters[key];
+                    var fieldFilter = { 'fieldName': key, 'filterValue': obj };
+                    filterCriteria.filters.push(fieldFilter);
+                }
+            } else {
+                filterCriteria = {};
+            }
+            var deferred = $q.defer();
+            $http.post(baseUrl + 'Dataset/GetDataset', filterCriteria)
+                .then(function (result) {
+                    // Successful
+                    deferred.resolve(result.data);
+                },
+                    function (result) {
+                        // Error
+                        deferred.reject(result);
+                    });
+            return deferred.promise;
+        };
+        return {
+            tupleList: tupleList,
+            getList: getList,
+            getTuple: getTuple,
+            saveTuple: saveTuple,
+            filterDataset: filterDataset
+        };
+    };
+    datasetService.$inject = ["$http", "$q", "baseUrl"];
+
+    var module = angular.module('DatasetVisualiser'); //MachineLearner?
+    module.factory('datasetService', datasetService);
+})();
--- MachineLearner/Scripts/Angular Controllers/datasetVisualiserIndexController.js
+++ MachineLearner/Scripts/Angular Controllers/datasetVisualiserIndexController.js
@@ -2,11 +2,12 @@
 (function () {
     'use strict';
 
-    var datasetVisualiserIndexController = function ($scope, $modal, $q, $http, $location, $window, $timeout, dialogs, model, exportCsv, dialogInfo, gridHeaderSetRepository, machineRepository, customModals, baseUrl, popoverTemplate, popoverTemplateGrid) {
-        $scope.headers = [
+    var datasetVisualiserIndexController = function ($scope, $modal, $q, $http, $location, $window, $timeout, dialogs, model, exportCsv, dialogInfo, gridHeaderSetRepository, dataRepository, customModals, baseUrl, popoverTemplate, popoverTemplateGrid) {
+
+        $scope.headers[0] = [
             {
-                title: 'Machine Number',
-                value: 'machineNumber',
+                title: 'Output - X',
+                value: 'datasetOutput',
                 visible: true,
                 anchor: true,
                 enableGrouping: false,
@@ -15,152 +16,23 @@
                 alignment: 'left',
                 width: 180,
                 type: 'none'
-            },
-            {
-                title: 'Description',
-                value: 'descr',
-                visible: true,
-                anchor: true,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'left',
-                width: 200,
-                type: 'none'
-            },
-            {
-                title: 'Alias',
-                value: 'alias',
-                visible: true,
-                anchor: true,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'left',
-                width: 140,
-                type: 'none'
-            },
-            {
-                title: 'Machine Group',
-                value: 'machineGroup',
-                visible: true,
-                anchor: true,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'left',
-                width: 140,
-                type: 'none'
-            },
-            {
-                title: 'Machine Enabled',
-                value: 'machineEnable',
-                visible: true,
-                anchor: false,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'center',
-                width: 140,
-                type: 'bool'
-            },
-            {
-                title: 'Date Range Enabled',
-                value: 'dateRangeEnable',
-                visible: false,
-                anchor: false,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'center',
-                width: 140,
-                type: 'bool'
-            },
-            {
-                title: 'Date Range End',
-                value: 'dateRangeEnd',
-                visible: false,
-                anchor: false,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'left',
-                width: 140,
-                type: 'datetime'
-            },
-            {
-                title: 'Date Range Start',
-                value: 'dateRangeStart',
-                visible: false,
-                anchor: false,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'left',
-                width: 140,
-                type: 'datetime'
-            },
-            {
-                title: 'Created By',
-                value: 'createdBy',
-                visible: true,
-                anchor: false,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'left',
-                width: 170,
-                type: 'none'
-            },
-            {
-                title: 'Date Created',
-                value: 'dateCreated',
-                visible: true,
-                anchor: false,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'left',
-                width: 180,
-                type: 'datetime'
-            },
-            {
-                title: 'Last Modified By',
-                value: 'lastModifiedBy',
-                visible: false,
-                anchor: false,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'left',
-                width: 140,
-                type: 'none'
-            },
-            {
-                title: 'Date Last Modified',
-                value: 'dateLastModified',
-                visible: true,
-                anchor: false,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'left',
-                width: 180,
-                type: 'datetime'
-            },
-            {
-                title: 'Purchase Date',
-                value: 'purchaseDate',
-                visible: false,
-                anchor: false,
-                enableGrouping: false,
-                groupedOn: false,
-                input: 'none',
-                alignment: 'left',
-                width: 140,
-                type: 'datetime'
             }
+
         ];
+
+        for (var i = 1; i < dataRepository.columns.length; i++) {
+            $scope.headers[i] = {
+                title: "Column " + i,
+                visible: true,
+                anchor: true,
+                enableGrouping: false,
+                groupedOn: false,
+                input: 'none',
+                alignment: 'left',
+                width: 180,
+                type: 'none'
+            };
+        }
 
         gridController.call(this, $scope, model, dialogs, gridHeaderSetRepository, dialogInfo);
 
@@ -171,7 +43,7 @@
             $scope.csvHeaders = exportCsv.csvHeaders;
             var deferred = $q.defer();
             $scope.filterCriteria.isCsvExport = true;
-            $http.post(baseUrl + "Machines/GetMachines", { filterCriteria: $scope.filterCriteria, fieldSet: exportCsv.getFieldSetForCsvExport($scope.headers) })
+            $http.post(baseUrl + "Dataset/GetDataset", { filterCriteria: $scope.filterCriteria, fieldSet: exportCsv.getFieldSetForCsvExport($scope.headers) })
                 .then(function (result) {
                     // Successful
                     $scope.filterCriteria.isCsvExport = false;
@@ -181,7 +53,7 @@
                         .then(function (data) {
                             return data;
                         });
-                },
+                    },
                     function (result) {
                         // Error
                         $scope.filterCriteria.isCsvExport = false;
@@ -200,29 +72,6 @@
         $scope.BooleanFilters = [{ name: "All", id: "" }, { name: "Yes", id: "true" }, { name: "No", id: "false" }];
 
         $scope.isAll = false;
-
-        $scope.selectAllRows = function () {
-            //check if all selected or not
-            if ($scope.isAll === false) {
-                //set all row selected
-                angular.forEach($scope.machines, function (row, index) {
-                    $scope.tableSelection.push({ machineNumber: $scope.machines[index].machineNumber, description: $scope.machines[index].descr });
-                });
-                $scope.isAll = true;
-            } else {
-                //set all row unselected
-                $scope.tableSelection = [];
-                $scope.isAll = false;
-            }
-        };
-
-        $scope.selectTrue = function (machineNumber) {
-            for (var i = 0; i < $scope.tableSelection.length; i++)
-                if ($scope.tableSelection[i].machineNumber === machineNumber) {
-                    return true;
-                }
-            return false;
-        }
 
         $scope.shouldBeDisabled = function (title) {
             if (title === 'Select') {
@@ -276,21 +125,6 @@
             return visbleHeaders;
         };
 
-        // Save Table Columns State
-        $scope.updateTableColumnsWidth = {
-        };
-
-        $scope.confirmGridReset = function () {
-            $scope.deleteGridSettings("Machines");
-        };
-
-        $scope.updateTableColumnState = function () {
-            var gridHeaderSet = {
-                gridTable: { tableName: "Machines" }
-            };
-            $scope.updateGridSettings(gridHeaderSet);
-        };
-
         $scope.toggleColumns = function (fieldName) {
             var column, i;
             for (i = 0; i < $scope.headers.length; i++) {
@@ -300,22 +134,22 @@
                 }
             };
             return true;
-        };
+        }; //???
 
         $scope.totalPages = 0;
         $scope.totalItems = 0;
 
         $scope.fetchResult = function () {
             $scope.selectedMachineNumber = null;
-            return machineRepository.filterMachines($scope.filterCriteria).then(function (data) {
-                $scope.machines = data.machines;
+            return dataRepository.filterData($scope.filterCriteria).then(function (data) {
+                $scope.dataset = data.dataset;
                 $scope.totalPages = Math.ceil(data.totalItems / $scope.filterCriteria.pageSize);
                 $scope.totalItems = data.filterCriteria.totalItems;
                 $scope.tableWidth = $scope.calculateTableWidth();
                 angular.element("table").colResizable({ disable: true });
                 angular.element("table").colResizable({});
             }, function () {
-                $scope.machines = [];
+                $scope.dataset = [];
                 $scope.totalPages = 0;
                 $scope.totalItems = 0;
             });
@@ -350,145 +184,10 @@
 
         $scope.selectPage(1);
         $scope.user = {};
-
-        $scope.copyMachine = function (e) {
-            $('#page-container').block({ message: "Loading..." });
-            var machineNumber = e.target.getAttribute('data-value');
-            window.location = "/Machines/Copy/" + machineNumber;
-        };
-
-        $scope.editMachine = function (e) {
-            $('#page-container').block({ message: "Loading..." });
-            var machineNumber = e.target.getAttribute('data-value');
-            window.location = "/Machines/Edit/" + machineNumber;
-        };
-
-        function createListFromMachineNumbers(obj) {
-            var str = '<ul class="xbreadcrumbs">';
-            for (var i in obj) {
-                str += '<li><a id="' + obj[i].machineNumber + '" href="/Machines/Edit/' + obj[i].machineNumber + '">' + obj[i].description + ' (' + obj[i].machineNumber + ')</a><input type="hidden" name="hdnMachineNumber' + i + '" value="' + obj[i].machineNumber + '"</li>';
-            }
-            str += '</ul>';
-            return str;
-        }
-
-        var modalTemplate = "";
-        var id = "";
-
-        $scope.editMultipleMachines = function () {
-            if (!isEmpty($scope.tableSelection)) {
-                var str = createListFromMachineNumbers($scope.tableSelection);
-                $(".modal-body #machineNumbers").html(str);
-                modalTemplate = $('#modal-content').html(); // make a copy of empty modal to reset form
-                $('#editMultipleMachinesModal').modal('show');
-            }
-            else
-                dialogInfo.showDialog("Please select the machines you wish to edit.", "error");
-        }
-
-        $scope.editMultipleVendingMachineAssignments = function () {
-            if (!isEmpty($scope.tableSelection)) {
-                var str = createListFromMachineNumbers($scope.tableSelection);
-                $(".modal-body #VMAmachineNumbers").html(str);
-                if ($scope.tableSelection.length === 1) {
-                    id = $scope.tableSelection[0].machineNumber;
-                    checkBoxListButtonPress('location-assignments', false, id, 'LocationAssignmentsLoading', '/machines/CheckBoxListOps');
-                    $('#editVendingMachinesModal').modal('show');
-                }
-                else {
-                    id = null;
-                    checkBoxListButtonPress('location-assignments', false, null, 'LocationAssignmentsLoading', '/machines/CheckBoxListOps');
-                    $('#editVendingMachinesModal').modal('show');
-                }
-            }
-            else
-                dialogInfo.showDialog("Please select the machines you wish to edit.", "error");
-        };
-
-        $('#editMultipleMachinesModal').on('hidden.bs.modal', function () {
-            $('#modal-content').html(modalTemplate);
-        });
-
-        $('#editVendingMachinesModal').on('hidden.bs.modal', function () {
-            vmaArray = [];
-        });
-
-        $scope.showDeleteAuditModal = function () {
-            $("#deleteHistoryModal").modal('show');
-        }
-
-        $(document).on("click", "#btnDeleteMachines", function (e) {
-            $('#editMultipleMachinesModal').modal('hide');
-            $scope.deleteMachines();
-        });
-
-        $(document).on("click", "#btnMultiMachineSave", function (e) {
-            $.ajax({
-                url: "Machines/EditMultipleMachineDetails",
-                type: "post",
-                data: $("form").serialize(),
-                success: function (result) {
-                    $('#editMultipleMachinesModal').modal('hide');
-                    dialogInfo.showDialog("Records updated successfully.", "success");
-                    $scope.fetchResult();
-                },
-                // failed
-                error: function () {
-                    $('#editMultipleMachinesModal').modal('hide');
-                    dialogInfo.showDialog("Unable to edit selected records, the records may be in use. If the problem persists then please contact support.", 'error');
-                }
-            });
-        });
-
-        $(document).on("click", "#btnMultiLocationAssignmentsSave", function (e) {
-            $.ajax({
-                url: "Machines/EditMultipleLocationAssignments",
-                type: "post",
-                data: $("form").serialize(),
-                success: function (result) {
-                    $('#editVendingMachinesModal').modal('hide');
-                    dialogInfo.showDialog("Records updated successfully.", "success");
-                    $scope.fetchResult();
-                },
-                // failed
-                error: function () {
-                    $('#editVendingMachinesModal').modal('hide');
-                    dialogInfo.showDialog("Unable to edit selected records, the records may be in use. If the problem persists then please contact support.", 'error');
-                }
-            });
-        });
-
-        // Delete machines
-        $scope.deleteMachines = function () {
-            if (!isEmpty($scope.tableSelection)) {
-                var str = createListFromMachineNumbers($scope.tableSelection);
-                var dlg = dialogs.confirm("Please Confirm", "Are you sure that you want to delete the following machines?" + str, { size: "sm", keyboard: true, backdrop: false, windowClass: "my-class" });
-                dlg.result.then(function (btn) {
-                    // Delete from database
-                    machineRepository.deleteMachines($scope.tableSelection).then(
-                        // success
-                        function () {
-                            $scope.fetchResult();
-                            $('#editMultipleMachinesModal').modal('hide');
-                            $scope.tableSelection = [];
-                            dialogInfo.showDialog("Records deleted successfully.", "success");
-                        },
-                        // failed
-                        function () {
-                            dialogInfo.showDialog("Unable to delete machines, records may be in use. If the error persists please contact support.", "error");
-                        });
-                }, function (btn) {
-                    // Just ignore
-                });
-            }
-            else {
-                dialogInfo.showDialog("Please select the machines you wish to delete.", "error");
-            }
-        };
     }
     //datasetVisualiserIndexController.$inject = ["$scope", "$modal", "$location", "$window", "$timeout", "$http", "dialogs", "model", "exportCsv", "dialogInfo", "gridHeaderSetRepository", "machineRepository", "customModals", "baseUrl", "popoverTemplate", "popoverTemplateGrid"];
-    var module = angular.module('PortalMVCApp');
-    angular.module('PortalMVCApp').config(function ($sceProvider) {
+    var module = angular.module('DatasetVisualiser'); //MachineLearner?
+    angular.module('DatasetVisualiser').config(function ($sceProvider) {
         $sceProvider.enabled(false);
     });
     module.controller("datasetVisualiserIndexController", datasetVisualiserIndexController);
