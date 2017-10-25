eec78fcce627bd14205492f041730cf0503c0372
ng1e16 <n.giannopoulos@soton.ac.uk>
2017-10-25 15:59:12 +0100

    added work database credentials

--- MachineLearner/DB/DB_Connection.cs
+++ MachineLearner/DB/DB_Connection.cs
@@ -6,7 +6,8 @@ namespace MachineLearner.DB
     class DB_Connection
     {
         public static SqlConnection NewCon;
-        public static string ConStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        //public static string ConStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public static string ConStr = ConfigurationManager.ConnectionStrings["WorkConnection"].ConnectionString;
 
         public static SqlConnection GetConnection()
         {
